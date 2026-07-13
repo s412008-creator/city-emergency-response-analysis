d7073f6b3231b67734c0949024b270d96fc22f16
莊子進 <zhuangzijin@zhuangzijindeMacBook-Air.local>
2026-07-13 23:18:18 +0800

    chore: Update base path for city-emergency-response-analysis and fix GH Pages white screen

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/city-ai-agent/',
+  base: '/city-emergency-response-analysis/',
   plugins: [
     react(),
     VitePWA({
